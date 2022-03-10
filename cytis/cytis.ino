30de4a931823b0d648f6a1f6e22646b75d01d7cb
sipandrei <pontos.serban@gmail.com>
2022-03-10 21:20:16 +0200

    added primordial :) command for bluetooth communication

--- cytis/cytis.ino
+++ cytis/cytis.ino
@@ -1,12 +1,18 @@
+#include <SoftwareSerial.h>
+
 const int pressurePin = A0;
-float pressureValue = 0;
-float currentPressurePsi = 0;
-float pressureVolts = 0;
 const float voutLower = 0.5;
 const float voutUpper = 4.5;
 const int transducerMaxPsi = 174;
-int targetPressure;
 const int intakePin1 = 2, intakePin2 = 3, exhaustPin1 = 4, exhaustPin2 = 5;
+const int tx = 7, rx = 6;
+float pressureValue = 0;
+float currentPressurePsi = 0;
+float pressureVolts = 0;
+int targetPressure;
+char serialInput[16];
+
+SoftwareSerial ble(rx, tx);
 
 float analogToPsi(float analogValue){
   pressureVolts = analogValue * 5.0 / 1023.0;
@@ -37,7 +43,14 @@ void valveCycle(int valvePin) {
   closeValve(valvePin);
 }
 
-int wholePart(float number){
+void pinsLow() {
+  digitalWrite(intakePin1, LOW);
+  digitalWrite(intakePin2, LOW);
+  digitalWrite(exhaustPin1, LOW);
+  digitalWrite(exhaustPin2, LOW);
+}
+
+int wholePart(float number) {
   int wholeNum;
   wholeNum = number;
   return wholeNum;
@@ -58,19 +71,40 @@ void initializeTargetPressure() {
 
 void setup(){
   Serial.begin(9600);
+  ble.begin(9600);
+  ble.println("Connected to Cytis");
   Serial.print("Welcome to Cytis\n");
+  pinMode(tx, OUTPUT);
+  pinMode(rx, INPUT);
   pinMode(intakePin1, OUTPUT);
   pinMode(intakePin2, OUTPUT);
   pinMode(exhaustPin1, OUTPUT);
   pinMode(exhaustPin2, OUTPUT);
+  pinsLow();
   initializeTargetPressure();
 }
 
+void commandProcessing(char command[]) {
+  ble.print(command);
+  if(strcmp(command, "currentPressure")){
+    ble.print(currentPressurePsi);
+    ble.println(" psi");
+  }
+}
+
 void loop(){
+  int letter = 0;
   pressureValue = analogRead(pressurePin);
   currentPressurePsi = analogToPsi(pressureValue);
   //pressureAdjust(intakePin1, exhaustPin1);
-  valveCycle(intakePin1);
-  pressureDebugDisplay();
+  //pressureDebugDisplay();
+  if(ble.available() > 0){
+    while(ble.available() > 0){
+      serialInput[letter] = ble.read();
+      letter++;
+    }
+    serialInput[letter]='\0';
+    commandProcessing(serialInput);
+  }
   delay(500);
 }
